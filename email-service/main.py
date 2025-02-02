7a6626cc8dc58c0020b38cd05c219bb76465ef0d
nishidh@rpi-5 <shekhawatnishidh5@gmail.com>
2025-02-03 01:22:27 +0530

    Add languages to Email

--- email-service/main.py
+++ email-service/main.py
@@ -41,7 +41,7 @@ def download_from_gcs(bucket_name, source_blob_name, destination_file_path):
 
 
 
-def send_message(service, sender, receiver, subject, first_name, last_name, body_header, body_content, language = "English",media_path=None):
+def send_message(service, sender, receiver, subject, first_name, last_name, body_header, body_content, language = "English",media_path=None,single_play_url=None):
     """Send an email message through Gmail API with an inline image or GIF, formatted HTML body, and footer"""
     try:
         # Create the MIME message
@@ -49,6 +49,43 @@ def send_message(service, sender, receiver, subject, first_name, last_name, body
         message["To"] = receiver
         message["From"] = sender
         message["Subject"] = subject
+
+        if language == "Spanish":
+          hey = "Hola,"
+          fan_community = "Actualizaciones de la Comunidad de Fans"
+          fan_community_description = "¡Únete a la conversación con otros fanáticos! Mira las últimas discusiones, encuestas y envíos de los fans."
+          fan_community_button = "Unirse a la Conversación"
+          
+          visit_mlb = "Visita Mi MLB"
+          
+          footer_modify = "Estás recibiendo este correo electrónico porque te registraste en nuestro MLB Fan Digest. Si deseas cancelar la suscripción o modificarla "
+          click_here = "haz clic aquí"
+          footer_poweredby = "Este resumen te lo trae Google Cloud - Gemini 2.0."
+        elif language == "Japanese":
+          hey = "こんにちは、"
+          fan_community = "ファンコミュニティの更新情報"
+          fan_community_description = "他のファンとの会話に参加しよう！最新のディスカッション、投票、ファンからの投稿をチェックしてみてください。"
+          fan_community_button = "ディスカッションに参加する"
+          
+          visit_mlb = "私のMLBを訪問"
+          
+          footer_modify = "このメールは、MLB Fan Digestに登録したために受信しています。購読を解除したり、設定を変更したりするには "
+          click_here = "こちらをクリック"
+          footer_poweredby = "このダイジェストはGoogle Cloud - Gemini 2.0によって提供されています。"
+        else:
+          hey = "Hey,"
+          fan_community = "Fan Community Updates"
+          fan_community_description = "Join the conversation with other fans! Check out the latest discussions, polls, and fan submissions."
+          fan_community_button = "Join the Discussion"
+          
+          visit_mlb = "Visit My MLB"
+
+          footer_modify = "You're receiving this email because you signed up for our MLB Fan Digest. If you'd like to unsubscribe or modify subscriptions "
+          click_here = "click here"
+          footer_poweredby = "This digest is Powered to you by Google Cloud - Gemini 2.0."
+        
+              
+
 
         # HTML body with dynamic content and embedded media
          # HTML body with dynamic content and embedded media (image or gif)
@@ -134,26 +171,22 @@ def send_message(service, sender, receiver, subject, first_name, last_name, body
           </head>
           <body>
             <div class="container">
-              <h1>Welcome, {first_name} {last_name}!</h1>
-              <p>We're thrilled to bring you this personalized digest of MLB updates just for you!</p>
-              <p>{body_header}</p>
+              <h1>{hey} {first_name}!</h1>
               
-              <h2>Latest Highlights</h2>
-              <div class="image-container">
-                <img src="cid:my_media" alt="Highlight" />
-              </div>
+              <h2>{body_header}</h2>
+              {f'<div class="image-container"><img src="cid:my_media" alt="Highlight" /></div>' if media_path else ''}
               <p>{body_content}</p>
-              
-              <h2>Fan Community Updates</h2>
-              <p>Join the conversation with other fans! Check out the latest discussions, polls, and fan submissions.</p>
+              {f"<div style='text-align: center;'><a href='{single_play_url}' class='cta-button'>View Clips of the play</a></div>" if single_play_url else''}
+              <h2>{fan_community}</h2>
+              <p>{fan_community_description}</p>
               <div style="text-align: center;">
-              <a href="https://www.mlb.com/fans" class="cta-button">Join the Discussion</a>
-              <a href="https://mlb-frontend-service-513391239750.asia-south1.run.app" class="cta-button">Visit My MLB</a>
+              <a href="https://www.mlb.com/fans" class="cta-button">{fan_community_button}</a>
+              <a href="https://mlb-frontend-service-513391239750.asia-south1.run.app" class="cta-button">{visit_mlb}</a>
               </div>
 
               <div class="footer">
-                <p>You're receiving this email because you signed up for our MLB Fan Digest. If you'd like to unsubscribe or modify subscriptions <a href="https://mlb-frontend-service-513391239750.asia-south1.run.app">click here</a>.</p>
-                <p>This digest is Powered to you by Google Cloud - Gemini 2.0.</p>
+                <p>{footer_modify}<a href="https://mlb-frontend-service-513391239750.asia-south1.run.app">{click_here}</a>.</p>
+                <p>{footer_poweredby}</p>
               </div>
             </div>
           </body>
@@ -223,6 +256,7 @@ def send_email(request):
         body_content = request_json.get('body_content')
         language = request_json.get('language')
         media_url = request_json.get('media_url')
+        play_id = request_json.get('play_id')
 
     if not sender or not receiver:
         return f'{{"error": "Sender and receiver email addresses are required."}}', 400
@@ -231,10 +265,15 @@ def send_email(request):
       media_path = download_from_gcs('mlb-objects',media_url,"./image.jpg")
     else:
       media_path = None
+
+    if play_id != "":
+      single_play_url = f'https://www.mlb.com/video/search?q=playid=\"{play_id}\"'
+    else:
+      single_play_url = None
       
     try:
         service = get_gmail_service()
-        message_id = send_message(service, sender, receiver, subject, first_name,last_name,body_header,body_content,language,media_path)
+        message_id = send_message(service, sender, receiver, subject, first_name,last_name,body_header,body_content,language,media_path,single_play_url)
 
         return f'{{"message": "{message_id}"}}', 200
     except Exception as e:
